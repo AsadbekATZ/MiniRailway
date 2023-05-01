2643414694e4ea0e342aaa3bb3a1fb8cdada70d2
Asadbek Ziyodullayev <ziyodullayevasadbek@gmail.com>
2023-05-01 21:31:59 +0500

    some thymeleaf changes

--- src/main/java/com/example/MiniRailway/repository/UserRepository.java
+++ src/main/java/com/example/MiniRailway/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package com.example.MiniRailway.repository;
 
 import com.example.MiniRailway.domain.entity.user.UserEntity;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -13,6 +15,8 @@ public interface UserRepository extends JpaRepository<UserEntity, UUID> {
 
     Optional<UserEntity> findByUsername(String username);
 
+    @Modifying
+    @Transactional
     @Query("update users u set u.balance=:balance where u.id=:id")
     void fillBalance(Double balance, UUID id);
 
